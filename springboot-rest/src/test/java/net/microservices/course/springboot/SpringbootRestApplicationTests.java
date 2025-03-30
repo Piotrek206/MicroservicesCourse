cc08b64142b29b4c7058d4dca14b14a084b7d4e9
Piotrek206 <piotrek206@onet.pl>
2025-03-30 18:49:03 +0200

    Building CRUD REST API's with MySQL Database

---
+++ springboot-rest/src/test/java/net/microservices/course/springboot/SpringbootRestApplicationTests.java
@@ -0,0 +1,13 @@
+package net.microservices.course.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
